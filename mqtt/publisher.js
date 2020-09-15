7c27f623b4b7e612fe1b01d0f206fde3c3e3364d
Hidemaro Fujinami <hdmrf.fjnm@gmail.com>
2020-09-15 15:57:04 +0900

    localhost pub/sub

--- mqtt/publisher.js
+++ mqtt/publisher.js
@@ -1,17 +1,19 @@
 var mqtt = require('mqtt');
-var mqtturl = 'mqtt://d12d677a:4f1245329c3363c8@broker.shiftr.io'
+var mqtturl = 'mqtt://192.168.0.180'
 
 console.log('connecting to: ' + mqtturl);
 var client = mqtt.connect(mqtturl, {clientId: 'nodejs-pub'});
 
 client.on('connect', () => {
   console.log('publisher connected.');
+
+  var i = 0;
+  setInterval(() => {
+    var msg = i.toString();
+    client.publish('topic0', msg);
+    i += 1;
+    console.log('publisher published:', i);
+  }, 10);
+
 });
 
-var i = 0;
-setInterval(() => {
-  var msg = i.toString();
-  client.publish('topic0', msg);
-  i += 1;
-  console.log('publisher published:', i);
-}, 10);
